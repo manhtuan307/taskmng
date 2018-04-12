ca6712d34dda2382dc856b24b9440e84ac3e2ff8
s3corp\tuan.nguyenm <tuan.nguyenm@s3corp.com.vn>
2018-04-12 19:21:47 +0700

    Using upper case for Json data

--- dto/LoginResult.go
+++ dto/LoginResult.go
@@ -2,9 +2,9 @@ package dto
 
 // A LoginResult represents authentication result together with token
 type LoginResult struct {
-	IsSuccess   bool   `json:"isSuccess"`
-	Message     string `json:"message"`
-	UserID      string `json:"userId"`
-	Token       string `json:"token"`
-	ExpiredTime string `json:"expiredTime"`
+	IsSuccess   bool   `json:"IsSuccess"`
+	Message     string `json:"Message"`
+	UserID      string `json:"UserId"`
+	Token       string `json:"Token"`
+	ExpiredTime string `json:"ExpiredTime"`
 }
